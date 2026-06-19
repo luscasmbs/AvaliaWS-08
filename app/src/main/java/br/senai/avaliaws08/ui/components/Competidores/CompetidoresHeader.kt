90d189bb1a48c463b5f6c6a1f4033ddf339ee6c1
luscasmbs <lucasmiguelbsilva@gmail.com>
2026-06-19 12:50:30 -0300

    feat: header da tela de competidores

---
+++ app/src/main/java/br/senai/avaliaws08/ui/components/Competidores/CompetidoresHeader.kt
@@ -0,0 +1,66 @@
+package br.senai.avaliaws08.ui.components.Competidores
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NavigateBefore
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import br.senai.avaliaws08.ui.theme.Blue40
+import br.senai.avaliaws08.ui.theme.Gray40
+
+@Composable
+fun CompetidoresHeader(navController: NavController){
+    Column(modifier = Modifier.fillMaxWidth().background(Gray40)) {
+        Row( modifier = Modifier.padding(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically){
+            IconButton(
+                onClick ={
+                    navController.popBackStack()
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.NavigateBefore,
+                    contentDescription = "Voltar tela"
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Competidores",
+                    fontSize = 18.sp,
+                    color = Blue40
+                )
+                Text(
+                    text = "4 cadastrados",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.ExtraLight,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CompetidoresHeaderPreview(){
+    CompetidoresHeader(navController = rememberNavController())
+}
